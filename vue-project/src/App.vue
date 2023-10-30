a32d57e8321fee04eae198213be51fdf82424ad1
dominik.halvonik <dominik.halvonik@gmail.com>
2023-10-30 17:03:36 +0100

    v-if, v-for, computed

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,10 +3,32 @@
     <p>Count is: {{ counter.count }}</p><br>
     <p><button @click="increment">Increment</button></p>
     <input v-model="text" placeholder="Type here"><br>
-    <p>{{ text }}</p><br>
+    <p>{{ text }}</p><br><br><br>
+
+    <button @click="toggle">Ukaz</button>
+    <h1 v-if="mega">Vue je super!!!!!!!!!!!</h1>
+    <h1 v-else>React je super!!!!!!!!!!!!</h1>
+    <br><br>
+    <form @submit.prevent="addTodo">
+        <input v-model="newTodo">
+        <button>Add todo</button>
+    </form>
+    <br>
+    <ul>
+        <li v-for="todo in filteredTodos" :key="todo.id">
+            <input type="checkbox" v-model="todo.done">
+            <span :class="{done: todo.done}">{{ todo.text }}</span>
+            <button @click="removeTodo(todo)">X</button>
+        </li>
+    </ul>
+    <button @click="hideCompleted = !hideCompleted">
+        {{ hideCompleted ? 'Ukaz vsetko' : 'Schovaj vsetko' }}
+    </button>
 </template>
 
 <script>
+let id = 0
+
 export default {
     data() {
         return {
@@ -15,13 +37,38 @@ export default {
                 count: parseInt(localStorage.getItem("counter")) || 0
             },
             titleStyle: 'title',
-            text: ''
+            text: '',
+            mega: true,
+            newTodo: '',
+            todos: [
+                {id: id++, text: 'Uprata izbu', done: true},
+                {id: id++, text: 'Vyniest smeti', done: true},
+                {id: id++, text: 'Ucit sa', done: false}
+            ],
+            hideCompleted: false,
+        }
+    },
+    computed: {
+        filteredTodos() {
+            return this.hideCompleted
+            ? this.todos.filter((t) => !t.done)
+            :this.todos
         }
     },
     methods: {
         increment() {
             this.counter.count++
             localStorage.setItem("counter", this.counter.count)
+        },
+        toggle() {
+            this.mega = !this.mega
+        },
+        addTodo() {
+            this.todos.push({id: id++, text: this.newTodo, done: false})
+            this.newTodo = ''
+        },
+        removeTodo(todo) {
+            this.todos = this.todos.filter((t) => t !== todo)
         }
     }
 }
@@ -30,5 +77,9 @@ export default {
 <style>
 .title {
     color: red;
+}
+
+.done {
+    text-decoration: line-through;
 }
 </style>
