345db32eab91721d581c5101cd8c7aa9130e7f2b
dominik.halvonik <dominik.halvonik@gmail.com>
2023-11-06 17:26:05 +0100

    Props / Emit / Slot

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,45 +1,30 @@
 <template>
-    <p>Todo id: {{ todoId }}</p>
-    <button @click="todoId++">Ziskaj novy todo</button>
-    <p v-if="!todoData">Loading...</p>
-    <pre v-else>{{ todoData }}</pre>
+  <h2>Toto je root component</h2><br>
+  <ChildComponent :msg="greeting" /> <br>
+  <ChildComponent @response="(msg) => childMsg = msg" /> <br>
+  <p>{{ childMsg }}</p>
+  <SlotComponent>{{ slotMessage }}</SlotComponent>
 </template>
 
 <script>
 
+import ChildComponent from "@/components/ChildComponent.vue";
+import SlotComponent from "@/components/SlotComponent.vue";
 export default {
     data() {
         return {
-            todoId: 1,
-            todoData: null
+            greeting: 'Pozdravujem s rodicovskeho komponentu',
+            childMsg: 'Nemam ziadnu spravu od svojich deti :(',
+            slotMessage: 'Slot je najlepsia funkcionalita v komponente'
         }
     },
-    methods: {
-        async fetchData() {
-            this.todoData = null
-            //AJAX - Asynchronous Javascript and XML
-            //AJAJ - Asynchronous Javascript and JSON
-            const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${this.todoId}`)
-            this.todoData = await res.json()
-        }
-    },
-    mounted() {
-        this.fetchData()
-    },
-    watch: {
-        todoId() {
-            this.fetchData()
-        }
+    components: {
+        ChildComponent,
+        SlotComponent
     }
 }
 </script>
 
 <style>
-.title {
-    color: red;
-}
 
-.done {
-    text-decoration: line-through;
-}
 </style>
---
+++ vue-project/src/components/ChildComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ msg || 'Nemam ziadne propsy' }}</h1>
+</template>
+
+<script>
+export default {
+    props: {
+        msg: String
+    },
+    emits: ['response'],
+    created() {
+        this.$emit('response', 'Toto je sprava z child komponentu')
+    }
+}
+</script>
+
