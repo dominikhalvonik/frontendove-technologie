a4b5c63b272dabbdab0cd73ce750ee2a45d05438
dominik.halvonik <dominik.halvonik@gmail.com>
2023-11-27 16:46:52 +0100

    State managemnt / Pinia

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,25 +1,44 @@
 <template>
   <div class="about">
-    <h1 @click="goToHomePage">This is an {{ message }} page</h1>
+    <h1 @click="goToHomePage">This is an {{ message }} page</h1><br>
+    <button @click="increment">Klikni ma</button><br>
+    <p>Počet kliknutí: {{ count }}</p>
   </div>
 
     <router-view />
 </template>
 
 <script lang="ts">
+import {useCounterOptionsStore} from "@/stores/counter-options";
+
 export default {
+    data() {
+        const counterStore = useCounterOptionsStore();
+
+        return {
+            // Vráti priamo reaktívnu referenciu na počet
+            counterStore,
+        };
+    },
     props: {
         message: String
     },
     computed: {
         routerText() {
             return this.$route.params.message
-        }
+        },
+        // Vypočítaná vlastnosť pre získanie aktuálneho počtu
+        count() {
+            return this.counterStore.count;
+        },
     },
     methods: {
         goToHomePage() {
           this.$router.push("/")
-        }
+        },
+        increment() {
+            this.counterStore.increment();
+        },
     }
 }
 </script>
