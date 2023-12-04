e96dcfd653916cff3c993f9cdd3f50141fb736b4
dominik.halvonik <dominik.halvonik@gmail.com>
2023-12-04 15:35:07 +0100

    Axios

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -3,6 +3,8 @@
     <h1 @click="goToHome">This is an {{ text }} page</h1><br>
     <button @click="increment">Klikni ma</button><br>
     <p>Počet kliknutí: {{ count }}</p>
+    <p>Result: {{ info }}</p>
+    <p>Result 2: {{ info2 }}</p>
   </div>
 
   <router-view></router-view>
@@ -11,6 +13,8 @@
 <script lang="ts">
 
 import {useCounterOptionsStore} from "@/stores/counter-options";
+import axios from "axios";
+import api from "@/axios/axios-instance";
 
 export default {
     data() {
@@ -19,6 +23,8 @@ export default {
         return {
             // Vráti priamo reaktívnu referenciu na počet
             counterStore,
+            info: "",
+            info2: ""
         };
     },
     props: {
@@ -32,6 +38,35 @@ export default {
         count() {
             return this.counterStore.count;
         },
+    },
+    created() {
+        axios.get("https://jsonplaceholder.typicode.com/todos/1")
+            .then(response => {
+                this.info = response.data;
+            }).catch(function (error) {
+                console.log(error)
+            })
+
+       /* api.get("todos/2")
+            .then(response => {
+                this.info2 = response.data;
+            }).catch(function (error) {
+            console.log(error)
+        })*/
+        const data = {
+            email: "fsdfdsafdsaffdsfdsafdsa@gmail.com",
+            password: "123456789aA",
+            game_token: "QhsSExGkWtNZT8nA5DymX0riBgaq1YFof4IKbCpJdjPLR3czM7Vu2e69wlHvUO",
+            language: "sk",
+            last_id_group: null,
+            id_group: null
+        }
+        api.post("oauth/register", data)
+            .then(response => {
+                this.info = response.data;
+            }).catch(function (error) {
+            console.log(error)
+        })
     },
     methods: {
         goToHome() {
